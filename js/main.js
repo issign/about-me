dd8a12946146aba33f8e5b14550fc68329865fc2
KihoLee <kiho2734@naver.com>
2025-05-17 20:07:51 +0900

    Add scroll animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log("test");
+const navHeader = document.querySelector(".navbar");
+const scrollWatcher = document.createElement("div");
+
+scrollWatcher.setAttribute("data-scroll-watcher", "");
+navHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries) => {
+  navHeader.classList.toggle("sticking", !entries[0].isIntersecting);
+});
+
+navObserver.observe(scrollWatcher);
